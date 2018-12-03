c92022f97a45bf2a0fe21b40cef8ad0a5c7b581d
rhules <38119764+rhules@users.noreply.github.com>
2018-12-02 23:57:00 -0800

    VEr. work faster

--- dgemm-optimize.c
+++ dgemm-optimize.c
@@ -18,29 +18,35 @@ void dgemm( int m, int n, float *A, float *C )
 	{
 		for(k = 0; k < n; k++ ) 
 		{		
-			for(j = 0; j < m-3; j+=4 )
+			for(j = 0; j < m-2; j+=3 )
 			{
 				C[i+j*m] += A[i+k*m] * A[j+k*m];
-				C[i+(j+3)*m] += A[i+k*m] * A[(j+3)+k*m];
-				C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
-				C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
+				// if(j + 3 < m){
+				// 	C[i+(j+3)*m] += A[i+k*m] * A[(j+3)+k*m];
+				// }
+				if(j + 2 < m){
+					C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
+				}
+				if(j + 1 < m){
+					C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
+				}
 			}
 		}
 	}
-	switch(m-j){
-		case 3:
-		{
-			C[i+(j+3)*m] += A[i+k*m] * A[(j+3)+k*m];
-		}
-		case 2:
-		{
-			C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
-		}
-		case 1:
-		{
-			C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
-		}
-	}
+	// switch(m-j){
+	// 	case 3:
+	// 	{
+	// 		C[i+(j+3)*m] += A[i+k*m] * A[(j+3)+k*m];
+	// 	}
+	// 	case 2:
+	// 	{
+	// 		C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
+	// 	}
+	// 	case 1:
+	// 	{
+	// 		C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
+	// 	}
+	// }
 		
 }
 
