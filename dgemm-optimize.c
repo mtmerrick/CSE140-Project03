82e61fe811a2118abf71a1e9797ecccdf3b43e6d
rhules <38119764+rhules@users.noreply.github.com>
2018-12-03 00:04:42 -0800

    Ver make clean

--- dgemm-optimize.c
+++ dgemm-optimize.c
@@ -18,7 +18,7 @@ void dgemm( int m, int n, float *A, float *C )
 	{
 		for(k = 0; k < n; k++ ) 
 		{		
-			for(j = 0; j < m; j+=4 )
+			for(j = 0; j < m; j+=3 )
 			{
 				C[i+j*m] += A[i+k*m] * A[j+k*m];
 				// if(j + 4 < m){
@@ -27,12 +27,12 @@ void dgemm( int m, int n, float *A, float *C )
 				// 	C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
 				// 	C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
 				// }
-				/*else */if(j + 3 < m){
-					C[i+(j+3)*m] += A[i+k*m] * A[(j+3)+k*m];
-					C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
-					C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
-				}
-				else if(j + 2 < m){
+				// /*else */if(j + 3 < m){
+				// 	C[i+(j+3)*m] += A[i+k*m] * A[(j+3)+k*m];
+				// 	C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
+				// 	C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
+				// }
+				/*else */if(j + 2 < m){
 					C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
 					C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
 				}
