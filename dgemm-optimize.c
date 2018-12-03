11b8f89466501046173b36ff2c306d426ecf57db
rhules <38119764+rhules@users.noreply.github.com>
2018-12-02 23:35:47 -0800

    Ver. fucking.work

--- dgemm-optimize.c
+++ dgemm-optimize.c
@@ -18,7 +18,7 @@ void dgemm( int m, int n, float *A, float *C )
 	{
 		for(k = 0; k < n; k++ ) 
 		{		
-			for(j = 0; j < m-3; j+=4 )
+			for(j = 0; j < m; j+=4 )
 			{
 				C[i+j*m] += A[i+k*m] * A[j+k*m];
 				C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
@@ -27,36 +27,36 @@ void dgemm( int m, int n, float *A, float *C )
 			}
 		}
 	}
-	switch(m-j){
-		case 3:
-		{
-			C[i+(j)*m] += A[i+k*m] * A[(j)+k*m];
-			C[i+(j)*m] += A[i+k*m] * A[(j)+k*m];
-			C[i+(j)*m] += A[i+k*m] * A[(j)+k*m];
-			break;
-		}
-		case 2:
-		{
-			C[i+(j-1)*m] += A[i+k*m] * A[(j-1)+k*m];
-			C[i+(j)*m] += A[i+k*m] * A[(j)+k*m];
-			C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
-			break;
-		}
-		case 1:
-		{
-			C[i+(j-2)*m] += A[i+k*m] * A[(j-2)+k*m];
-			C[i+(j-1)*m] += A[i+k*m] * A[(j-1)+k*m];
-			C[i+(j)*m] += A[i+k*m] * A[(j)+k*m];
-			break;
-		}
-		case 0:
-		{
-			C[i+(j-3)*m] += A[i+k*m] * A[(j-3)+k*m];
-			C[i+(j-2)*m] += A[i+k*m] * A[(j-2)+k*m];
-			C[i+(j-1)*m] += A[i+k*m] * A[(j-1)+k*m];
-			break;
-		}
-	}
+	// switch(m-j){
+	// 	case 3:
+	// 	{
+	// 		C[i+(j)*m] += A[i+k*m] * A[(j)+k*m];
+	// 		C[i+(j)*m] += A[i+k*m] * A[(j)+k*m];
+	// 		C[i+(j)*m] += A[i+k*m] * A[(j)+k*m];
+	// 		break;
+	// 	}
+	// 	case 2:
+	// 	{
+	// 		C[i+(j-1)*m] += A[i+k*m] * A[(j-1)+k*m];
+	// 		C[i+(j)*m] += A[i+k*m] * A[(j)+k*m];
+	// 		C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
+	// 		break;
+	// 	}
+	// 	case 1:
+	// 	{
+	// 		C[i+(j-2)*m] += A[i+k*m] * A[(j-2)+k*m];
+	// 		C[i+(j-1)*m] += A[i+k*m] * A[(j-1)+k*m];
+	// 		C[i+(j)*m] += A[i+k*m] * A[(j)+k*m];
+	// 		break;
+	// 	}
+	// 	case 0:
+	// 	{
+	// 		C[i+(j-3)*m] += A[i+k*m] * A[(j-3)+k*m];
+	// 		C[i+(j-2)*m] += A[i+k*m] * A[(j-2)+k*m];
+	// 		C[i+(j-1)*m] += A[i+k*m] * A[(j-1)+k*m];
+	// 		break;
+	// 	}
+	// }
 		
 }
 
