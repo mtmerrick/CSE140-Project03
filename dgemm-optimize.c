93650b66677b0003d81d2aa32fc94b89dee09ba4
rhules <38119764+rhules@users.noreply.github.com>
2018-12-03 00:09:43 -0800

    Ver. closerr.....

--- dgemm-optimize.c
+++ dgemm-optimize.c
@@ -18,7 +18,7 @@ void dgemm( int m, int n, float *A, float *C )
 	{
 		for(k = 0; k < n; k++ ) 
 		{		
-			for(j = 0; j < m-1; j+=3 )
+			for(j = 0; j < m-2; j+=3 )
 			{
 				C[i+j*m] += A[i+k*m] * A[j+k*m];
 				C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
