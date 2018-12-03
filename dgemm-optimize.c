81b0667ded74ee2359f6a7e38e02c17e90204eb6
rhules <38119764+rhules@users.noreply.github.com>
2018-12-02 23:40:52 -0800

    Ver. WORK

--- dgemm-optimize.c
+++ dgemm-optimize.c
@@ -25,12 +25,10 @@ void dgemm( int m, int n, float *A, float *C )
 					case 3:
 					{
 						C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
-						break;
 					}
 					case 2:
 					{
 						C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
-						break;
 					}
 					case 1:
 					{
