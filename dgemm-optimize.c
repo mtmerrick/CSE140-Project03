cad56c5b8779ad392b409618d161d83900576999
rhules <38119764+rhules@users.noreply.github.com>
2018-12-02 23:02:38 -0800

    Ver. maybe.something

--- dgemm-optimize.c
+++ dgemm-optimize.c
@@ -13,11 +13,12 @@
 //Loop Unrolling
 void dgemm( int m, int n, float *A, float *C )
 {
+	int i,j,k;
 	for( int i = 0; i < m; i++ )
 	{
 		for( int k = 0; k < n; k++ ) 
 		{		
-			for( int j = 0; j < m-3; j+=4 )
+			for(j = 0; j < m-3; j+=4 )
 			{
 				C[i+j*m] += A[i+k*m] * A[j+k*m];
 				C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
@@ -26,6 +27,9 @@ void dgemm( int m, int n, float *A, float *C )
 			}
 		}
 	}
+	C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
+	C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
+	C[i+(j+3)*m] += A[i+k*m] * A[(j+3)+k*m];
 }
 
 //Matrix Padding
