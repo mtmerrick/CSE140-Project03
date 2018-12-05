b4d75dbe5b252d2716e25f19322432e8dd381185
rhules <38119764+rhules@users.noreply.github.com>
2018-12-04 17:17:33 -0800

    Ver. Murderous Intent

--- dgemm-optimize.c
+++ dgemm-optimize.c
@@ -4,71 +4,71 @@
 #include <stdio.h>
 
 //SSE
-// void dgemm( int m, int n, float *A, float *C )
-// {
-// 	for( int i = 0; i < m; i++ ) {
-// 		for( int k = 0; k < n; k++ ) {
-// 			for( int j = 0; j < m; j++ ){
-// 				__m128 primus = _mm_load_ss(A + (j + k * m));
-// 				__m128 secundus = _mm_load_ss(A + (i + k * m));
-// 				__m128 tertius = _mm_load_ss(C + (i + j * m));
-// 				__m128 quartus = _mm_mul_ss(primus, secundus);
-// 				__m128 quintus = _mm_add_ss(tertius, quartus);
-// 				_mm_store_ss(C + (i + j * m), quintus);
-// 			}
-// 		}
-// 	}
-// }
-
-
-//Loop Unrolling
 void dgemm( int m, int n, float *A, float *C )
 {
-	int i,j,k;
-	for(i = 0; i < m; i++ )
-	{
-		for(k = 0; k < n; k++ ) 
-		{		
-			for(j = 0; j < m; j+=5 )
-			{
-				C[i+j*m] += A[i+k*m] * A[j+k*m];
-				if(j + 4 < m){
-					C[i+(j+4)*m] += A[i+k*m] * A[(j+4)+k*m];
-					C[i+(j+3)*m] += A[i+k*m] * A[(j+3)+k*m];
-					C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
-					C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
-				}
-				else if(j + 3 < m){
-					C[i+(j+3)*m] += A[i+k*m] * A[(j+3)+k*m];
-					C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
-					C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
-				}
-				else if(j + 2 < m){
-					C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
-					C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
-				}
-				else if(j + 1 < m){
-					C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
-				}
+	for( int i = 0; i < m; i++ ) {
+		for( int k = 0; k < n; k++ ) {
+			for( int j = 0; j < m; j++ ){
+				__m128 primus = _mm_load_ss(A + (j + k * m));
+				__m128 secundus = _mm_load_ss(A + (i + k * m));
+				__m128 tertius = _mm_load_ss(C + (i + j * m));
+				__m128 quartus = _mm_mul_ss(primus, secundus);
+				__m128 quintus = _mm_add_ss(tertius, quartus);
+				_mm_store_ss(C + (i + j * m), quintus);
 			}
 		}
 	}
-	// switch(m-j){
-	// 	case 3:
-	// 	{
-	// 		C[i+(j+3)*m] += A[i+k*m] * A[(j+3)+k*m];
-	// 	}
-	// 	case 2:
-	// 	{
-	// 		C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
-	// 	}
-	// 	case 1:
-	// 	{
-	// 		C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
-	// 	}
-	// }
-		
 }
+
+
+//Loop Unrolling
+// void dgemm( int m, int n, float *A, float *C )
+// {
+// 	int i,j,k;
+// 	for(i = 0; i < m; i++ )
+// 	{
+// 		for(k = 0; k < n; k++ ) 
+// 		{		
+// 			for(j = 0; j < m; j+=5 )
+// 			{
+// 				C[i+j*m] += A[i+k*m] * A[j+k*m];
+// 				if(j + 4 < m){
+// 					C[i+(j+4)*m] += A[i+k*m] * A[(j+4)+k*m];
+// 					C[i+(j+3)*m] += A[i+k*m] * A[(j+3)+k*m];
+// 					C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
+// 					C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
+// 				}
+// 				else if(j + 3 < m){
+// 					C[i+(j+3)*m] += A[i+k*m] * A[(j+3)+k*m];
+// 					C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
+// 					C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
+// 				}
+// 				else if(j + 2 < m){
+// 					C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
+// 					C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
+// 				}
+// 				else if(j + 1 < m){
+// 					C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
+// 				}
+// 			}
+// 		}
+// 	}
+// 	// switch(m-j){
+// 	// 	case 3:
+// 	// 	{
+// 	// 		C[i+(j+3)*m] += A[i+k*m] * A[(j+3)+k*m];
+// 	// 	}
+// 	// 	case 2:
+// 	// 	{
+// 	// 		C[i+(j+2)*m] += A[i+k*m] * A[(j+2)+k*m];
+// 	// 	}
+// 	// 	case 1:
+// 	// 	{
+// 	// 		C[i+(j+1)*m] += A[i+k*m] * A[(j+1)+k*m];
+// 	// 	}
+// 	// }
+		
+// }
 
 //Cache Blocking
 // void dgemm( int m, int n, float *A, float *C )
